752711e1a9db95c799138427d0e547a09e25c2c7
Akvaro03 <68175898+Akvaro03@users.noreply.github.com>
2021-08-23 12:17:15 -0300

    .

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-ScrollReveal().reveal('section', { delay: 100, reset: true });
-ScrollReveal().reveal('.container2', { delay: 500, reset: true });
+ScrollReveal().reveal('#titulo', { delay: 100, reset: true });
+ScrollReveal().reveal('.titulo2', { delay: 300, reset: true });
+ScrollReveal().reveal('.subtitulo2', { delay: 700, reset: true });
+anime({
+    targets: '#titulo',
+    translateX: 0,
+});
